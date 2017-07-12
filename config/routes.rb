418c21d4aa5104071f52737a8084513e1cb9a8e7
Anthony <Knoll>
2017-07-12 17:15:02 -0600

    Made routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/home'
 
-  get 'pages/welcome'
+  root to: 'pages#welcome'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
